4e28b60d9d1796d3ef7cab9377fc7a3dcabe91b0
YanaB-b <j.biliuk96@gmail.com>
2023-02-01 20:36:43 +0100

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,3 +3,30 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryBoxRef = document.querySelector('.gallery');
+const galleryBoxMarkup = createBoxItem(galleryItems);
+galleryBoxRef.insertAdjacentHTML('afterbegin',galleryBoxMarkup);
+
+function createBoxItem(galleryItems){
+    return galleryItems.map(({ preview, original, description }) =>
+    `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`)
+  .join('');
+}
+
+const galleryBoxModal = new SimpleLightbox('.gallery a',
+{
+    captionsData : 'alt',
+    captionsDelay: 300,
+})
