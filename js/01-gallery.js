6e585745f9d60af992898465c698c3479be3b17c
YanaB-b <j.biliuk96@gmail.com>
2023-02-02 21:06:26 +0100

    q

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,10 +1,8 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
-console.log(galleryItems);
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
 
--- js/03-feedback.js
+++ js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from "lodash.throttle";
 
+const KEY = "feedback-form-state";
+const form = document.querySelector(".feedback-form");
+
+form.addEventListener('submit', feedbackForm);
+form.addEventListener('input', throttle(onInput,500));
+
+
+
+const saveData = {};
+function onInput(event){
+    saveData[event.target.name]=event.targer.value;
+    localStorage.setItem(KEY, JSON.stringify(saveData));
+}
+
+setInput();
+function setInput(){
+    let getInput = localStorage.getItem(KEY);
+    if (getInput){
+        getInput = JSON.parse(getInput);
+        Object.entries(getInput).forEach(([name,value])=>{
+            saveData[name]=value;
+            form.elements[name].value = value;  
+        });  
+    
+    }
+}
+function feedbackForm(event){
+    event.preventDefault();
+    const formData = new FormData (form);
+    formData.forEach((value, name) =>
+    console.log(`${name}`, value));
+    event.targer.reset ();
+    localStorage.removeItem(KEY);
+}
