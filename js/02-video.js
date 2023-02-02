794b60d34d470b3f56dc27dadc3eaaf80c270abf
YanaB-b <j.biliuk96@gmail.com>
2023-02-02 10:54:38 +0100

    ДЗ

--- js/02-video.js
+++ js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const KEY = 'videoplayer-current-time';
+
+
+videoTimer();
+
+const setPlayer = throttle(function(iframe){
+    localStorage.setPlayer(KEY,`${iframe.seconds}` );
+},1000);
+
+
+player.on('timeupdate', setPlayer);
+    
+function videoTimer(){
+    const timerStorage = localStorage.getItem(KEY);
+    if (timerStorage){
+        player.setCurrentTime(localStorage.getItem(KEY));
+
+    }
+}
